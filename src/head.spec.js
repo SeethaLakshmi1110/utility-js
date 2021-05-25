add44a9323616b9a7a60a6659ccc9f1df0d1aade
Seetha Lakshmi G <seethalakshmi.g@thoughtworks.com>
2021-05-25 20:55:01 +0530

    Added files for head function and related test cases

---
+++ src/head.spec.js
@@ -0,0 +1,13 @@
+const head = require('./head')
+
+describe('Head', () => {
+
+    it('Head of an empty list is null', () => {
+        expect(head([])).toBe(null);
+    });
+
+    it('Head of (1, 2, 3) is 1', () => {
+        expect(head([1, 2, 3])).toEqual(1);
+    });
+
+})
